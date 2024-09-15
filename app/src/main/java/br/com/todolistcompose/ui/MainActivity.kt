94c688443185144e8caa677878c2ea82eb618dfe
ronanlm10@hotmail.com <ronanlm10@hotmail.com>
2024-09-15 18:55:45 -0300

    first commit

---
+++ app/src/main/java/br/com/todolistcompose/ui/MainActivity.kt
@@ -0,0 +1,16 @@
+package br.com.todolistcompose.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import br.com.todolistcompose.ui.theme.ToDoListComposeTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ToDoListComposeTheme {}
+        }
+    }
+}
